e520f6fead9dee5a3359e33ea5c459fbc36555c8
jjeongneee <gardening0530@gmail.com>
2023-11-14 11:39:45 +0900

    글 수정시 writer Input  삭제

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -96,17 +96,7 @@ export function BoardEdit() {
           }
         />
       </FormControl>
-      <FormControl>
-        <FormLabel>작성자</FormLabel>
-        <Input
-          value={board.writer}
-          onChange={(e) =>
-            updateBoard((draft) => {
-              draft.writer = e.target.value;
-            })
-          }
-        />
-      </FormControl>
+
       <Button colorScheme="blue" onClick={onOpen}>
         저장
       </Button>
