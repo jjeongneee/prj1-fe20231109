5d8112051b76934853d8156d04da68bf92db86fb
jjeongneee <gardening0530@gmail.com>
2023-11-14 16:44:59 +0900

    LogInProvider에 로그인 기능 모아 넣기

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { MemberList } from "./page/member/MemberList";
 import { MemberView } from "./page/member/MemberView";
 import { MemberEdit } from "./page/member/MemberEdit";
 import { MemberLogin } from "./page/member/MemberLogin";
-import axios from "axios";
+import LogInProvider from "./component/LogInProvider";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -33,49 +33,11 @@ const routes = createBrowserRouter(
   ),
 );
 
-export const LoginContext = createContext(null);
-
 function App(props) {
-  const [login, setLogin] = useState("");
-
-  useEffect(() => {
-    fetchLogin();
-  }, []);
-
-  function fetchLogin() {
-    axios.get("/api/member/login").then((response) => setLogin(response.data));
-  }
-
-  function isAuthenticated() {
-    return login !== "";
-  }
-
-  // 로그인 된 계정이 aimin인지 확인
-  function isAdmin() {
-    if (login.auth) {
-      return login.auth.some((elem) => elem.name === "admin");
-    }
-    return false;
-  }
-
-  // function isManager() {
-  //   return login.auth.some((elem) => elem.name === "manager");
-  // }
-  //
-  // function hasAuth() {
-  //   return login.auth.some((elem) => elem.name === auth);
-  // }
-
-  function hasAccess(userId) {
-    return login.id === userId;
-  }
-
   return (
-    <LoginContext.Provider
-      value={{ login, fetchLogin, isAuthenticated, hasAccess, isAdmin }}
-    >
+    <LogInProvider>
       <RouterProvider router={routes} />;
-    </LoginContext.Provider>
+    </LogInProvider>
   );
 }
 
---
+++ src/component/LogInProvider.js
@@ -0,0 +1,51 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const LoginContext = createContext(null);
+function LogInProvider({ children }) {
+  const [login, setLogin] = useState("");
+
+  useEffect(() => {
+    fetchLogin();
+  }, []);
+
+  console.log(login);
+
+  function fetchLogin() {
+    axios.get("/api/member/login").then((response) => setLogin(response.data));
+  }
+
+  function isAuthenticated() {
+    return login !== "";
+  }
+
+  // 로그인 된 계정이 aimin인지 확인
+  function isAdmin() {
+    if (login.auth) {
+      return login.auth.some((elem) => elem.name === "admin");
+    }
+    return false;
+  }
+
+  // function isManager() {
+  //   return login.auth.some((elem) => elem.name === "manager");
+  // }
+  //
+  // function hasAuth() {
+  //   return login.auth.some((elem) => elem.name === auth);
+  // }
+
+  function hasAccess(userId) {
+    return login.id === userId;
+  }
+
+  return (
+    <LoginContext.Provider
+      value={{ login, fetchLogin, isAuthenticated, hasAccess, isAdmin }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
+}
+
+export default LogInProvider;
