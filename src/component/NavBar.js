8391c51e0a8076482fc914e0ca0dc1df57665c91
jjeongneee <gardening0530@gmail.com>
2023-11-16 16:33:33 +0900

    좋아요 기능 추가 중

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,6 +3,12 @@ import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useContext, useEffect } from "react";
 import { LoginContext } from "./LogInProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons/faHeart";
+import { faHouseChimney } from "@fortawesome/free-solid-svg-icons/faHouseChimney";
+import { faPenFancy } from "@fortawesome/free-solid-svg-icons/faPenFancy";
+import { faPerson } from "@fortawesome/free-solid-svg-icons";
 
 export function NavBar() {
   const { fetchLogin, login, isAuthenticated, isAdmin } =
@@ -35,9 +41,15 @@ export function NavBar() {
 
   return (
     <Flex>
-      <Button onClick={() => navigate("/")}>home</Button>
+      <Button onClick={() => navigate("/")}>
+        <FontAwesomeIcon icon={faHouseChimney} style={{ color: "#d6712e" }} />
+        home
+      </Button>
       {isAuthenticated() && (
-        <Button onClick={() => navigate("/write")}>write</Button>
+        <Button onClick={() => navigate("/write")}>
+          <FontAwesomeIcon icon={faPenFancy} style={{ color: "#907dd4" }} />
+          write
+        </Button>
       )}
       {isAuthenticated() || (
         <Button onClick={() => navigate("/signup")}>signup</Button>
@@ -47,11 +59,15 @@ export function NavBar() {
       )}
       {isAuthenticated() && (
         <Button onClick={() => navigate("/member?" + urlParams.toString())}>
+          <FontAwesomeIcon icon={faPerson} style={{ color: "#99c8e5" }} />
           회원정보
         </Button>
       )}
       {isAuthenticated() || (
-        <Button onClick={() => navigate("/login")}>로그인</Button>
+        <Button onClick={() => navigate("/login")}>
+          <FontAwesomeIcon icon={faHeart} style={{ color: "#ee7cd5" }} />
+          로그인
+        </Button>
       )}
       {isAuthenticated() && <Button onClick={handleLogout}>로그아웃</Button>}
     </Flex>
